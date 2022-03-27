dd4719dae25e803afc92ee85acf0b4ee0fd4a09f
Kenichi Takahashi <takahashi.kenichi@gmail.com>
2022-03-27 15:59:06 +0000

    Update Application Configration

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.hosts << ".amazonaws.com"
+    config.hosts << ".herokuapp.com"
+
   end
 end
