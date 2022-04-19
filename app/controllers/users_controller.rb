1ec56e478c299b1fbe9d8a41c16354162d572754
Kenichi Takahashi <takahashi.kenichi@gmail.com>
2022-04-19 15:37:20 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
+#    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user.activated?
   end
 
   def new
@@ -18,9 +20,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -74,5 +76,9 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
-
+    
+    # 有効なユーザかどうか確認
+    def acitivated_user
+      redirect_to(root_url) unless current_user.activated?
+    end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,17 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  # host = 'example.com' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
+
+  # クラウドIDEの場合は以下をお使いください
+  host = 'b04ef9f844524802b891a8858a871abd.vfs.cloud9.ap-northeast-1.amazonaws.com'     # クラウドIDE
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
+  # localhostで開発している場合は以下をお使いください
+  # host = 'localhost:3000'                     # ローカル環境
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  
 
   config.action_mailer.perform_caching = false
 
